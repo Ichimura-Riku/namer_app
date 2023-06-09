705581f2ab1ad880ec9ea538a0803a9d8cdcc061
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-06-09 11:47:52 +0900

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,15 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
+
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
         ],
+
       ),
     );
   }
