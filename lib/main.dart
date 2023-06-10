2a59efb9441bdc00f51dace1fc3ab435e031f945
Ichimura-Riku <c1103865@planet.kanazawa-it.ac.jp>
2023-06-10 14:48:09 +0900

    change UIlayout

--- lib/main.dart
+++ lib/main.dart
@@ -27,24 +27,63 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
+
 class MyHomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {           // ← 1
-    var appState = context.watch<MyAppState>();  // ← 2
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current; // ← Add this.
 
-    return Scaffold(                             // ← 3
-      body: Column(                              // ← 4
-        children: [
-          Text('A random AWESOME idea:'),        // ← 5
-          Text(appState.current.asLowerCase),    // ← 6
-          ElevatedButton(
-            onPressed: () {
-              print('button pressed!');
-            },
-            child: Text('Next'),
-          ),
-        ],                                       // ← 7
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('A random AWESOME idea:'),
+            BigCard(pair: pair), // ← Change to this.
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      elevation: 10,
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
